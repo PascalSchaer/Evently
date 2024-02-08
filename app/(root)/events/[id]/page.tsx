290be6e75d7f03aea891a7cc54ffd28475363439
Pascal <pascalschaer8@gmail.com>
2024-02-08 15:59:45 -0700

    implement Stripe

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import { formatDateTime } from "@/lib/utils";
 import Collection from "@/components/shared/Collection";
+import CheckoutButton from "@/components/shared/CheckoutButton";
 
 const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
   const event = await getEventById(id);
@@ -51,6 +52,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
           </div>
 
           {/*  CHECKOUT BUTTON   */}
+          <CheckoutButton event={event} />
 
           <div className="flex flex-col gap-5">
             <div className="flex gap-2 md:gap-3">
